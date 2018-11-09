78f697c8f31da939c484681feef9358db3262255
jennieann <mail@jennie-ann.se>
2018-11-09 16:28:57 +0100

    Remove puppies from routes

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Recept API' });
 });
-var db = require('../queries');
-router.get('/api/puppies', db.getAllPuppies);
-router.get('/api/puppies/:id', db.getSinglePuppy);
-router.post('/api/puppies', db.createPuppy);
-router.put('/api/puppies/:id', db.updatePuppy);
-router.delete('/api/puppies/:id', db.removePuppy);
-
-router.get('/api/kennels', db.getAllKennels);
-router.get('/api/kennels/:id/pups', db.getAllPuppiesByKennel);
 
 var db = require('../queries');
 router.get('/api/recipes', db.getAllRecipes);
